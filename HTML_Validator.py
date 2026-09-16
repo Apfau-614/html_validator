469da23e0c6cd895edb26d55ae2d16a2fca2a458
Mike Izbicki <mike@izbicki.me>
2026-09-16 09:37:19 -0700

    update

--- HTML_Validator.py
+++ HTML_Validator.py
@@ -13,9 +13,9 @@ def validate_html(html):
 
     # HINT:
     # use the _extract_tags function below to generate a list of html tags without any extra text;
-    # then process these html tags using the balanced parentheses algorithm from the class/book
-    # the main difference between your code and the code from class will be that you will have to keep track of not just the 3 types of parentheses,
-    # but arbitrary text located between the html tags
+    # then process these html tags using the balanced parentheses algorithm from the stack.py file.
+    # The main difference between your code and the code from class will be that you will have to keep track of not just the 3 types of parentheses,
+    # but arbitrary text located between the html tags.
 
 
 def _extract_tags(html):
